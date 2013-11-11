dbe3b5a9607ff4ec1a32efe5cfeb24ef5affd3a1
Matthew Cattell <matthew.cattell@cfengine.com>
2013-11-11 15:19:03 +0100

    added script and components to create local debian repo with test content

---
+++ tests/acceptance/17_packages/testdebianpackage/dummy.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char ** argv)
+{
+ 
+    char * out = "This is a really complex piece of software!\n";
+    printf("My program says: %s", out);
+    return 0;
+}
