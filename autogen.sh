cdb4421ab7008362a7aa1d19038260de245688cd
Kristian Amlie <kristian.amlie@cfengine.com>
2014-07-14 14:29:26 +0200

    Switch to autotools to avoid maintaining ugly Makefile platform hacks.
    The existing approach does not work on AIX (and possibly others).
    
    (cherry picked from commit e3ac259e2b3fdfc12fbbf62fe1d8f7138885c7f7)
    
    Conflicts:
    	Makefile

---
+++ autogen.sh
@@ -0,0 +1,38 @@
+#
+try_exec() {
+  type "$1" > /dev/null 2>&1 && exec "$@"
+}
+
+unset foo
+(: ${foo%%bar}) 2> /dev/null
+T1="$?"
+
+if test "$T1" != 0; then
+  try_exec /usr/xpg4/bin/sh "$0" "$@"
+  echo "No compatible shell script interpreter found."
+  echo "Please find a POSIX shell for your system."
+  exit 42
+fi
+
+#
+# Do not set -e before switching to POSIX shell, as it will break the test
+# above.
+#
+set -e
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd $srcdir
+
+if ! test -d m4; then
+  mkdir m4
+fi
+
+autoreconf -Wno-portability --force --install -I m4 || exit 1
+cd $ORIGDIR || exit $?
+
+if [ -z "$NO_CONFIGURE" ]; then
+  $srcdir/configure --enable-maintainer-mode "$@"
+fi
