018a9ac8c85bc7ddb2c74a33feaf0fa32cc4b187
Dimitrios Apostolou <dimitrios.apostolou@cfengine.com>
2014-10-28 14:02:56 +0100

    Simplify script - no change in functionality.

--- autogen.sh
+++ autogen.sh
@@ -1,38 +1,12 @@
-#
-try_exec() {
-  type "$1" > /dev/null 2>&1 && exec "$@"
-}
+#!/bin/sh
 
-unset foo
-(: ${foo%%bar}) 2> /dev/null
-T1="$?"
-
-if test "$T1" != 0; then
-  try_exec /usr/xpg4/bin/sh "$0" "$@"
-  echo "No compatible shell script interpreter found."
-  echo "Please find a POSIX shell for your system."
-  exit 42
-fi
-
-#
-# Do not set -e before switching to POSIX shell, as it will break the test
-# above.
-#
-set -e
-
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
 
 ORIGDIR=`pwd`
-cd $srcdir
+srcdir=`dirname $0`
+[ -n "$srcdir" ] && cd $srcdir
 
-if ! test -d m4; then
-  mkdir m4
-fi
+[ ! -d m4 ] && mkdir m4
+autoreconf -Wno-portability --force --install -I m4  ||  exit $?
 
-autoreconf -Wno-portability --force --install -I m4 || exit 1
-cd $ORIGDIR || exit $?
-
-if [ -z "$NO_CONFIGURE" ]; then
-  $srcdir/configure --enable-maintainer-mode "$@"
-fi
+cd $ORIGDIR
+[ -z "$NO_CONFIGURE" ] && $srcdir/configure --enable-maintainer-mode "$@"
