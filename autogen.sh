8fbff28d506409c4994a9e678778fae8483ae9cb
Dimitrios Apostolou <dimitrios.apostolou@cfengine.com>
2016-09-16 17:44:13 +0200

    Update determine-version.py from Core.

--- autogen.sh
+++ autogen.sh
@@ -2,13 +2,21 @@
 
 
 ORIGDIR=`pwd`
+
 srcdir=`dirname $0`
 [ -n "$srcdir" ] && cd $srcdir
 
+echo "$0: Running determine-version.py ..."
+rm -f CFVERSION
+3rdparty/core/determine-version.py > CFVERSION \
+    || echo "$0: Unable to auto-detect CFEngine version, continuing"
+
+echo "$0: Running autoreconf ..."
 [ ! -d m4 ] && mkdir m4
 autoreconf -Wno-portability --force --install -I m4  ||  exit $?
 
-cd $ORIGDIR
+cd -               # back to original directory
+
 if [ -z "$NO_CONFIGURE" ]
 then
     $srcdir/configure --enable-maintainer-mode "$@"  ||  exit $?
