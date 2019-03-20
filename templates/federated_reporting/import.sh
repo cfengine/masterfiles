0030c8943be813eb34b321bd1b95309630a61064
Vratislav Podzimek <vratislav.podzimek@northern.tech>
2019-03-20 14:24:22 +0100

    Add scripts and templates for Federated Reporting
    The configurable parts are only in the config.sh.mustache and
    log.sh.mustache files, the other files just use the configuration
    rendered by the policy with those two templates.
    
    Ticket: ENT-4473
    Changelog: Added scripts and templates for Federated Reporting

---
+++ templates/federated_reporting/import.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -e
+
+# make sure a failure in any part of a pipe sequence is a failure
+set -o pipefail
+
+source "$(dirname "$0")/config.sh"
+source "$(dirname "$0")/log.sh"
+source "$(dirname "$0")/parallel.sh"
+
+# check that we have all the variables we need
+true "${CFE_BIN_DIR?undefined}"
+true "${CFE_FR_SUPERHUB_DROP_DIR?undefined}"
+true "${CFE_FR_SUPERHUB_IMPORT_DIR?undefined}"
+true "${CFE_FR_COMPRESSOR_EXT?undefined}"
+true "${CFE_FR_EXTRACTOR?undefined}"
+true "${CFE_FR_TABLES?undefined}"
+
+if ! type "$CFE_FR_EXTRACTOR" >/dev/null; then
+  log "Extractor $CFE_FR_EXTRACTOR not available!"
+  exit 1
+fi
+
+# TODO: we should do some validation of the files here
+mkdir -p "$CFE_FR_SUPERHUB_IMPORT_DIR"
+mv "$CFE_FR_SUPERHUB_DROP_DIR/"*".sql.$CFE_FR_COMPRESSOR_EXT" "$CFE_FR_SUPERHUB_IMPORT_DIR" || true
+
+dump_files="$(ls -1 "$CFE_FR_SUPERHUB_IMPORT_DIR/"*".sql.$CFE_FR_COMPRESSOR_EXT" 2>/dev/null)" ||
+  {
+    log "No files to import."
+    exit 0
+  }
+
+log "Database cleanup"
+{
+  cat<<EOF
+\set QUIET on
+\set ON_ERROR_STOP 1
+SET session_replication_role = replica;
+EOF
+  # XXX: this needs to be moved into the importing transaction once partitioning
+  #      is done
+  printf 'TRUNCATE %s;\n' $CFE_FR_TABLES
+} | "$CFE_BIN_DIR"/psql -U cfpostgres -d cfdb
+log "Database cleanup: DONE"
+
+# make sure the script we are about to run is executable
+chmod u+x "$(dirname "$0")/import_file.sh"
+
+log "Importing files: $dump_files"
+failed=0
+echo "$dump_files" | run_in_parallel "$(dirname "$0")/import_file.sh" - $CFE_FR_IMPORT_NJOBS ||
+  failed=1
+
+if [ "$failed" != "0" ]; then
+  log "Importing files: FAILED"
+  for file in "$CFE_FR_SUPERHUB_IMPORT_DIR/"*".sql.$CFE_FR_COMPRESSOR_EXT.failed"; do
+    log "Failed to import file '${file%%.failed}'"
+    rm -f "$file"
+  done
+  exit 1
+else
+  log "Importing files: DONE"
+fi
