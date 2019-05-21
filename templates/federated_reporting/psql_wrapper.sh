c4b8fe15b49673ae1da2759ee11d5e4f9e869ef2
Craig Comstock <craig.comstock@northern.tech>
2019-05-21 15:39:56 -0500

    ENT-4612: policy calls ensure_feeders(ARRAY) to ensure feeders are in database Changelog: none

--- templates/federated_reporting/psql_wrapper.sh
+++ templates/federated_reporting/psql_wrapper.sh
@@ -13,18 +13,17 @@ if [ $# -ne 2 ]; then
 fi
 
 TMP=$(mktemp)
-TMP2=$(mktemp)
 OUT=$(su - cfpostgres --command "psql --quiet --tuples-only --no-align --no-psqlrc \"$1\" --command=\"$2\"" 2> $TMP)
 RETURN_CODE=$?
 ERR=$(<$TMP)
 
-EXIT_CODE=$(echo $TMP | awk -F= '{ if ( /exit_code/ ) print $2}')
+EXIT_CODE=$(echo $OUT | awk -F= '{ if ( /exit_code/ ) print $2}')
 
-rm $TMP $TMP2
+rm $TMP
 
 if [ $RETURN_CODE -ne 0 ]; then
-    echo "$OUT"
-    echo "$ERR"
+    echo "stdout: $OUT"
+    echo "stderr: $ERR"
     exit 2; # failed
 fi
 
