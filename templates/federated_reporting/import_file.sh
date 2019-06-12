19eb95b08f5ff09c5c4914cf476eae4e3a500f34
Craig Comstock <craig.comstock@northern.tech>
2019-06-12 17:10:35 -0500

    ENT-4745: cleaned up output of import_file.sh Changelog: None
    (cherry picked from commit 5b94634880552b3664eca1d14ee9ca09ba63df2c)

--- templates/federated_reporting/import_file.sh
+++ templates/federated_reporting/import_file.sh
@@ -3,7 +3,6 @@
 #
 # $1 -- compressed SQL file to import
 set -e
-set -x
 
 source "$(dirname "$0")/config.sh"
 
@@ -22,31 +21,24 @@ hostkey=$(basename "$file" | cut -d. -f1)
 
 mv "$file" "$file.importing"
 
-  # temp step to see the result of sed filter
-  "$CFE_FR_EXTRACTOR" $CFE_FR_EXTRACTOR_ARGS "$file.importing" | sed $CFE_FR_SED_ARGS $(printf ' -f %s' $sed_scripts) > $file.after-sed
-
 {
   cat<<EOF
 \set ON_ERROR_STOP 1
 SET session_replication_role = replica;
 BEGIN;
-
 SELECT ensure_feeder_schema('$hostkey');
-
 SELECT current_schema(); -- debug info about which current schema
-
 -- we need to get to some types in public so change search_path
 SELECT pg_catalog.set_config('search_path', ( select current_schema() || ', public' ), false);
-
--- now we should be inside the proper schema or bombed out
-\i $file.after-sed
-
-SET SCHEMA 'public';
-
-SELECT attach_feeder_schema('$hostkey');
-
-COMMIT;
-
 EOF
-} | "$CFE_BIN_DIR"/psql -U $CFE_FR_DB_USER --echo-all -d cfdb >$file.log 2>&1 && rm -f "$file.importing" || mv "$file.importing" "$file.failed"
+
+  "$CFE_FR_EXTRACTOR" $CFE_FR_EXTRACTOR_ARGS "$file.importing" | sed $CFE_FR_SED_ARGS $(printf ' -f %s' $sed_scripts)
+
+cat<<EOF
+SET SCHEMA 'public';
+SELECT attach_feeder_schema('$hostkey');
+COMMIT;
+EOF
+} | "$CFE_BIN_DIR"/psql -U $CFE_FR_DB_USER --echo-all -d cfdb >$file.log 2>&1 &&
+  rm -f "$file.importing" || mv "$file.importing" "$file.failed"
 
