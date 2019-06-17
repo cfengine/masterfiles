9560bac269a83e3786f4cc815b756b02d89ccdf0
Craig Comstock <craig.comstock@northern.tech>
2019-06-17 11:47:16 -0500

    ENT-4772: use table whitelist and create feeder schema from superhub
    Switch back to using a whitelist of tables to dump/import.
    Generate feeder schema based on superhub schema, not via dumping the schema.
    
    Changelog: none
    (cherry picked from commit 983759de1fa04635c16a03c7b676f17d0520b789)

--- templates/federated_reporting/dump.sh
+++ templates/federated_reporting/dump.sh
@@ -32,17 +32,8 @@ failed=0
 ts="$(date -Iseconds)"  # ISO 8601 format that doesn't have spaces in it
 in_progress_file="$CFE_FR_DUMP_DIR/$CFE_FR_FEEDER_$ts.sql.$CFE_FR_COMPRESSOR_EXT.dumping"
 
-# switch to using --exclude-table=table or --tables
-# tables can include views, mat views, sequences, foreign tables
-# pattern according to the same rules used by psql's \d commands, include wildcard characters for __promiselog_* !!!
-# let's experiment with no --inserts and instead rely on the default of COPY, since that is much faster and due to partitioning should not be such a problem :)
-# so for a first go, just do a simple naive dump!!! `pg_dump cfdb` ---->
-# if I use the simple case I get all the functions without ifexists or anything, so try --clean and --if-exists
-# keep the plain text format for granular error reporting. :)
-# --no-owner for me as I am running this locally, wouldn't be a problem in production
-# <2019-06-09 Sun 21:47> remove --clean and --if-exists, the schema should always be empty to start with
 log "Dumping tables: $CFE_FR_TABLES"
-"$CFE_BIN_DIR"/pg_dump --no-owner cfdb |
+"$CFE_BIN_DIR"/pg_dump --inserts --data-only $(printf ' -t %s' $CFE_FR_TABLES) cfdb |
   "$CFE_FR_COMPRESSOR" $CFE_FR_COMPRESSOR_ARGS > "$in_progress_file" || failed=1
 
 if [ "$failed" != "0" ]; then
