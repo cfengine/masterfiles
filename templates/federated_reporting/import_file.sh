983759de1fa04635c16a03c7b676f17d0520b789
Craig Comstock <craig.comstock@northern.tech>
2019-06-17 11:47:16 -0500

    ENT-4772: use table whitelist and create feeder schema from superhub
    Switch back to using a whitelist of tables to dump/import.
    Generate feeder schema based on superhub schema, not via dumping the schema.
    
    Changelog: none

--- templates/federated_reporting/import_file.sh
+++ templates/federated_reporting/import_file.sh
@@ -12,6 +12,7 @@ true "${CFE_FR_SED_ARGS?undefined}"
 true "${CFE_FR_EXTRACTOR?undefined}"
 true "${CFE_FR_EXTRACTOR_ARGS?undefined}"
 true "${CFE_FR_DB_USER?undefined}"
+true "${CFE_FR_TABLES?undefined}"
 
 file="$1"
 
@@ -19,26 +20,30 @@ sed_scripts="$(ls -1 "$CFE_FR_SED_FILTERS_DIR/"*".sed" 2>/dev/null)"
 
 hostkey=$(basename "$file" | cut -d. -f1)
 
+table_whitelist=$(printf "'%s'," $CFE_FR_TABLES | sed -e 's/,$//')
+
 mv "$file" "$file.importing"
 
 {
   cat<<EOF
 \set ON_ERROR_STOP 1
-SET session_replication_role = replica;
 BEGIN;
-SELECT ensure_feeder_schema('$hostkey');
-SELECT current_schema(); -- debug info about which current schema
--- we need to get to some types in public so change search_path
-SELECT pg_catalog.set_config('search_path', ( select current_schema() || ', public' ), false);
+SELECT ensure_feeder_schema('$hostkey', ARRAY[$table_whitelist]);
 EOF
 
   "$CFE_FR_EXTRACTOR" $CFE_FR_EXTRACTOR_ARGS "$file.importing" | sed $CFE_FR_SED_ARGS $(printf ' -f %s' $sed_scripts)
 
 cat<<EOF
+
 SET SCHEMA 'public';
-SELECT attach_feeder_schema('$hostkey');
+SELECT attach_feeder_schema('$hostkey', ARRAY[$table_whitelist]);
 COMMIT;
 EOF
-} | "$CFE_BIN_DIR"/psql -U $CFE_FR_DB_USER --echo-all -d cfdb >$file.log 2>&1 &&
-  rm -f "$file.importing" || mv "$file.importing" "$file.failed"
+} | "$CFE_BIN_DIR"/psql -U $CFE_FR_DB_USER -d cfdb >$file.log 2>&1 && {
+  rm -f "$file.importing"
+  exit 0
+} || {
+  mv "$file.importing" "$file.failed"
+  exit 1
+}
 
