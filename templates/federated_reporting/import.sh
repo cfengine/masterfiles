998c953a584299a9c30634338f38688546e5d0ac
Craig Comstock <craig.comstock@northern.tech>
2020-07-24 15:43:58 -0500

    Added optional handle duplicates step in federated reporting import
    This feature removes duplicate hostkey entries during federated reporting
    import based on lastreporttimestamp in __hosts table.
    
    Enable this feature by defining a class in augments (def.json):
    
    {
      "classes": {
        "cfengine_mp_fr_handle_duplicate_hostkeys": ["any::"]
      }
    }
    
    Ticket: ENT-6035
    Changelog: title

--- templates/federated_reporting/import.sh
+++ templates/federated_reporting/import.sh
@@ -17,6 +17,7 @@ true "${CFE_FR_COMPRESSOR_EXT?undefined}"
 true "${CFE_FR_EXTRACTOR?undefined}"
 true "${CFE_FR_TABLES?undefined}"
 true "${CFE_FR_INVENTORY_REFRESH_CMD?undefined}"
+true "${CFE_FR_HANDLE_DUPLICATES?undefined}"
 
 if ! type "$CFE_FR_EXTRACTOR" >/dev/null; then
   log "Extractor $CFE_FR_EXTRACTOR not available!"
@@ -57,8 +58,10 @@ table_whitelist=$(printf "'%s'," $CFE_FR_TABLES | sed -e 's/,$//')
 
 failed=0
 log "Setting up schemas for import"
+declare -a hostkeys
 for file in $dump_files; do
   hostkey=$(basename "$file" | cut -d. -f1)
+  hostkeys+=($hostkey)
   "$CFE_BIN_DIR"/psql -U $CFE_FR_DB_USER -d cfdb --set "ON_ERROR_STOP=1" \
                       -c "SELECT ensure_feeder_schema('$hostkey', ARRAY[$table_whitelist]);" \
     > schema_setup.log 2>&1 || failed=1
@@ -98,6 +101,22 @@ else
     hostkey=$(basename "$file" | cut -d. -f1)
     "$CFE_BIN_DIR"/psql -U $CFE_FR_DB_USER -d cfdb -c "SELECT drop_feeder_schema('$hostkey');" || true
   done
+fi
+
+if [ "$CFE_FR_HANDLE_DUPLICATES" = "yes" ]; then
+  log "Handle Duplicate Hostkeys"
+  hostkey_list=$(printf "'%s'," ${hostkeys[*]} | sed -e 's/,$//')
+  "$CFE_BIN_DIR"/psql -U $CFE_FR_DB_USER -d cfdb --set "ON_ERROR_STOP=1" \
+                      -c "SELECT handle_duplicate_hostkeys_in_import(ARRAY[$hostkey_list]);"  \
+    > duplicates.log 2>&1 || failed=1
+  if [ "$failed" = "0" ]; then
+    log "Handle Duplicate Hostkeys: DONE"
+  else
+    log "Handle Duplicate Hostkeys: FAILED"
+    log "last 10 lines of duplicates.log"
+    tail -n 10 duplicates.log
+    exit 1
+  fi
 fi
 
 failed=0
