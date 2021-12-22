858a6358df2be504896983dcb0f87f266bc6929b
Vratislav Podzimek <vratislav.podzimek@northern.tech>
2021-12-22 11:51:53 +0100

    Replace cfbs/cleanup.sh with prepare.sh
    Requiring autotools for just converting a checked-out version of
    MPF into a directory that can actually be used as masterfiles is
    unfortunate. However, autotools deal with many things, especially
    on non-standard platforms, and provide many commonly used
    commands and features.
    
    To support the simple use case of just using the checked-out
    version of MPF as masterfiles this commit introduces a script
    that does the necessary bits:
    
      1. determine version and release
      2. expand values in templates
      3. remove unnecessary files
    
    And since such use case is not specific to cfbs, let's just have
    the script in the top-level directory.
    
    Ticket: CFE-3872
    Changelog: None
    (cherry picked from commit 23401ebf92174a88cb2fc2030d61d97a05335ab7)

---
+++ prepare.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+set -e
+
+if [ $# = 0 ] || [ $1 != "-y" ]; then
+  echo "WARNING: This script deletes files and directories, including itself."
+  echo "Never run it in your development clone of MPF!"
+  echo -n "Do you really want to continue? [y/N] "
+  read ans
+  if [ x$ans != "xy" ] && [ x$ans != "xY" ]; then
+    exit 0
+  fi
+fi
+
+cd "$(dirname $0)"
+
+echo "Running determine-version.sh ..."
+rm -f CFVERSION
+misc/determine-version.sh .CFVERSION > CFVERSION \
+    || echo "Unable to auto-detect CFEngine version, continuing"
+
+echo "Running determine-release.sh ..."
+rm -f CFRELEASE
+misc/determine-release.sh CFRELEASE \
+    || { echo "Unable to auto-detect CFEngine release, continuing"; echo 1 >CFRELEASE; }
+
+version=$(cat CFVERSION)
+release=$(cat CFRELEASE)
+prefix="/var/cfengine/"
+
+templates=$(find . -name .git -prune -o -name '*.in' -print)
+for template in $templates; do
+  sed -e "s|@VERSION@|$version|g" -e "s|@RELEASE@|$release|g" -e "s|@prefix@|$prefix|g" $template > ${template%%.in}
+done
+
+cat <<EOF | xargs rm -rf
+aclocal.m4
+autogen.sh
+autom4te.cache
+CFRELEASE
+.CFVERSION
+CFVERSION
+CHANGELOG.md
+CODE_OF_CONDUCT.org
+config.guess
+config.log
+config.status
+config.sub
+configure
+configure.ac
+CONTRIBUTING.md
+example_def.json
+.git
+.gitignore
+install-sh
+m4
+.mailmap
+Makefile
+Makefile.am
+Makefile.in
+misc
+missing
+MPF.md
+README.md
+test-driver
+tests
+travis-scripts
+.travis.yml
+
+$templates
+$(basename $0)
+EOF
